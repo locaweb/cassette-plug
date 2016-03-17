3b570adff0310aad39511e9e01793c4f4f167db9
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2016-03-17 15:29:41 -0300

    Release 1.0.0

--- mix.exs
+++ mix.exs
@@ -1,11 +1,15 @@
 defmodule Cassette.Plug.Mixfile do
   use Mix.Project
 
+  def version, do: "1.0.0"
+
   def project do
     [app: :cassette_plug,
-     version: "0.0.1",
+     version: version,
      elixir: "~> 1.2",
+     description: "An auth Plug using Cassette",
      elixirc_paths: elixirc_paths(Mix.env),
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      source_url: "https://github.com/locaweb/cassette-plug",
@@ -21,6 +25,15 @@ defmodule Cassette.Plug.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :cassette]]
+  end
+
+  def package do
+   [
+       files: ["lib", "mix.exs", "README.md", "LICENSE.md", "CONTRIBUTING.md"],
+       maintainers: ["Ricardo Hermida Ruiz"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/locaweb/cassette-plug",
+                "Docs" => "https://hexdocs.pm/cassette-plug/#{version}"}]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
